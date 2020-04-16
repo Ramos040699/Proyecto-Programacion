9ec12aa2ad09a46974e83bf8bdddb699fc50ed6d
PC <PC@RamosPC>
2020-04-16 17:42:11 -0600

    Primer commit

---
+++ src/proyecto/programacion/ProyectoProgramacion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.programacion;
+
+/**
+ *
+ * @author PC
+ */
+public class ProyectoProgramacion {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
