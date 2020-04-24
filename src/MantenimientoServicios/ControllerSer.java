ea046551c4084bde0641c9303026e9b6945f50e0
schav <schav@LAPTOP-4JPNB8PB>
2020-04-24 16:08:16 -0600

    Falta el controller

---
+++ src/MantenimientoServicios/ControllerSer.java
@@ -0,0 +1,24 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MantenimientoServicios;
+
+/**
+ *
+ * @author schav
+ */
+public class ControllerSer {
+     public void iniciar(int ID, int NumEmpleado, String Nombre, String Servicio){
+//      ServiciosM prueba = new ServiciosM();
+//      prueba.setID(txtID);
+//      prueba.setNumEmpleado(NumEmpleado);
+//      prueba.setNombre(Nombre);
+//      prueba.setServicio(Servicio);
+//      
+//      
+//      prueba.guardaremp(prueba);
+     } 
+}
---
+++ src/MantenimientoServicios/ServiciosM.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MantenimientoServicios;
+import java.util.ArrayList;
+import mantenimientoempl.Empleado;
+
+/**
+ *
+ * @author schav
+ */
+public class ServiciosM {
+    
+    //Atributos 
+    private int ID;
+    private int NumEmpleado;
+    private String Nombre;
+    private String Servicio;
+    
+    public ServiciosM
+        (int ID, int NumEmpleado , String Nombre, String Servicio){
+            
+            this.ID = ID;
+            this.NumEmpleado = NumEmpleado;
+            this.Nombre = Nombre;
+            this.Servicio = Servicio;
+        }
+        
+     
+     public int getID(){
+         return ID;
+     }
+     
+     public void setID(int ID){
+         this.ID = ID;
+     }
+  
+     
+     public int getNumEmpleados(){
+         return NumEmpleado;
+     }
+     
+     public void setNumEmpleado(int NumEmpleado){
+         this.NumEmpleado = NumEmpleado;
+     }
+     
+          
+     public String getNombre(){
+         return Nombre;
+     }
+     
+     public void setNombre(String Nombre){
+         this.Nombre = Nombre;
+     }
+
+    public String getServicio() {
+        return Servicio;
+    }
+
+    public void setServicio(String Servicio) {
+        this.Servicio = Servicio;
+    }
+    ArrayList <ServiciosM> list = new ArrayList();
+     
+     public void guardaremp(ServiciosM SerM){
+         list.add(SerM);
+     }
+    
+}
